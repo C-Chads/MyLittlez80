49985b45492754c934d37e23ab4aaa4ff5bad291
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-04 00:27:18 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -20,15 +20,21 @@
 #define MAXIMUM_STRING_LENGTH   100
 
 static void	emulate (char *filename);
+static void	emulate_test(char *filename);
 int main (int argc, char** argv)
-{
-	emulate(argv[1]);
-    return EXIT_SUCCESS;
+{int i;
+	for(i = 1; i < argc; i++){
+		if(argv[i-1][0] == '-' && argv[i-1][1] == 't')
+			{emulate_test(argv[i]);return EXIT_SUCCESS;}
+		else if(argv[i-1][0] == '-' && argv[i-1][1] == 'r')
+			{emulate(argv[i]);return EXIT_SUCCESS;}
+	}
+    return EXIT_FAILURE;
 }
 
 /* Emulate "zexdoc.com" or "zexall.com". */
 
-static void emulate (char *filename)
+static void emulate_test(char *filename)
 {
         FILE   	*file;
         long   	l;
@@ -44,39 +50,77 @@ static void emulate (char *filename)
         }
         fseek(file, 0, SEEK_END);
         l = ftell(file);
-
         fseek(file, 0, SEEK_SET);
         fread(context.memory + 0x100, 1, l, file);
         fclose(file);
+
+   		context.memory[0] = 0xd3;       /* OUT N, A */
+		context.memory[1] = 0x00;
+		context.memory[5] = 0xdb;       /* IN A, N */
+		context.memory[6] = 0x00;
+		context.memory[7] = 0xc9;       /* RET */
+		context.is_done = 0;
         /* Emulate. */
 		Z80Reset(&context.state);
 		context.state.pc = 0x100;
 		total = 0.0;
+		context.is_done = 0;
 	do{
 		total += Z80Emulate(&context.state, CYCLES_PER_STEP, &context);
-	}while(context.state.status != Z80_STATUS_HALT);
+	}while(context.state.status != Z80_STATUS_HALT && !context.is_done);
+}
+
+
+static void emulate(char *filename)
+{
+        FILE   	*file;
+        long   	l;
+	MY_LITTLE_Z80	context;
+        double 	total;
+        printf("Testing \"%s\"...\n", filename);
+        if ((file = fopen(filename, "rb")) == NULL) {
+
+                fprintf(stderr, "Can't open file!\n");
+                exit(EXIT_FAILURE);
+
+        }
+        fseek(file, 0, SEEK_END);
+        l = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        fread(context.memory + 0x100, 1, l, file);
+        fclose(file);
+		context.is_done = 0;
+        /* Emulate. */
+		Z80Reset(&context.state);
+		context.state.pc = 0x100;
+		total = 0.0;
+		context.is_done = 0;
+	do{
+		total += Z80Emulate(&context.state, CYCLES_PER_STEP, &context);
+	}while(context.state.status != Z80_STATUS_HALT && !context.is_done);
+}
+
+/*
+	TEMPORARY
+*/
+void out_impl(MY_LITTLE_Z80* mlz80, int port, unsigned char x)
+{
+	/*TODO*/
+	(void)port;
 }
 
 /* Emulate CP/M bdos call 5 functions 2 (output character on screen) and 9
  * (output $-terminated string to screen).
  */
-
-void out_impl(MY_LITTLE_Z80* mlz80, int port, unsigned char x)
+unsigned char in_impl(MY_LITTLE_Z80* mlz80, int port)
 {
-        if (zextest->state.registers.byte[Z80_C] == 2)
-                putchar(zextest->state.registers.byte[Z80_E]);
-        else if (zextest->state.registers.byte[Z80_C] == 9) {
-                int     i;
-                for (i = zextest->state.registers.word[Z80_DE];zextest->memory[i] != '$';i++) {
-                        putchar(zextest->memory[i & 0xffff]);
-                }
-        }
-}
-
-
-unsigned char in_impl(MY_LITTLE_Z80*MY_LITTLE_Z80, int port)
-{
-	(void)MY_LITTLE_Z80;
-	(void)port;
-   return 0; /*TODO*/
+	if (mlz80->state.registers.byte[Z80_C] == 2)
+		putchar(mlz80->state.registers.byte[Z80_E]);
+	else if (mlz80->state.registers.byte[Z80_C] == 9) {
+		int     i;
+		for (i = mlz80->state.registers.word[Z80_DE];mlz80->memory[i] != '$';i++) {
+			putchar(mlz80->memory[i & 0xffff]);
+		}
+	}
+	return 0;
 }
